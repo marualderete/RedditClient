dacd7f1140e768820978a602170d698b7d9ff0a8
Maria Alderete <maru.ferri@gmail.com>
2018-01-25 16:50:13 -0300

    First commit: The application is created

---
+++ MyRedditApp/MyRedditApp/MyRedditAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MyRedditApp
+{
+    public partial class MyRedditAppPage : ContentPage
+    {
+        public MyRedditAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
